debecf953b9c5c2492cfc4350d3367021b1325af
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-06 21:04:16 +0100

    add default export to 404 page

--- apps/dashboard-app/pages/404.tsx
+++ apps/dashboard-app/pages/404.tsx
@@ -1,44 +1,44 @@
-import { createStyles, Title, Text, Button, Container, Group, rem } from '@mantine/core';
+import { Button, Container, createStyles, Group, rem, Text, Title } from '@mantine/core';
 
-const useStyles = createStyles((theme) => ({
-  root: {
-    paddingTop: rem(80),
-    paddingBottom: rem(80),
-  },
+const useStyles = createStyles(theme => ({
+	root: {
+		paddingTop: rem(80),
+		paddingBottom: rem(80)
+	},
 
-  label: {
-    textAlign: 'center',
-    fontWeight: 900,
-    fontSize: rem(220),
-    lineHeight: 1,
-    marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
-    color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
+	label: {
+		textAlign: 'center',
+		fontWeight: 900,
+		fontSize: rem(220),
+		lineHeight: 1,
+		marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
+		color: theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2],
 
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: rem(120),
-    },
-  },
+		[theme.fn.smallerThan('sm')]: {
+			fontSize: rem(120)
+		}
+	},
 
-  title: {
-    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
-    textAlign: 'center',
-    fontWeight: 900,
-    fontSize: rem(38),
+	title: {
+		fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+		textAlign: 'center',
+		fontWeight: 900,
+		fontSize: rem(38),
 
-    [theme.fn.smallerThan('sm')]: {
-      fontSize: rem(32),
-    },
-  },
+		[theme.fn.smallerThan('sm')]: {
+			fontSize: rem(32)
+		}
+	},
 
-  description: {
-    maxWidth: rem(500),
-    margin: 'auto',
-    marginTop: theme.spacing.xl,
-    marginBottom: `calc(${theme.spacing.xl} * 1.5)`,
-  },
+	description: {
+		maxWidth: rem(500),
+		margin: 'auto',
+		marginTop: theme.spacing.xl,
+		marginBottom: `calc(${theme.spacing.xl} * 1.5)`
+	}
 }));
 
-export function NotFoundTitle() {
+function NotFoundTitle() {
   const { classes } = useStyles();
 
   return (
@@ -57,3 +57,5 @@ export function NotFoundTitle() {
     </Container>
   );
 }
+
+export default NotFoundTitle;
