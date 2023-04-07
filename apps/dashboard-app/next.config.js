c017a312fddbb8830b579454e5aa19330215c7ad
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-07 12:49:42 +0100

    revert next to v13.1.1

--- apps/dashboard-app/next.config.js
+++ apps/dashboard-app/next.config.js
@@ -12,8 +12,7 @@ const nextConfig = {
     // Set this to true if you would like to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
-  },
-  distDir: "build"
+  }
 };
 
 // @ts-ignore
