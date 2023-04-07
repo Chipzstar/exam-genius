e1897201e25eccde0ae460f7f8e1df3b05dd939c
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-08 00:49:41 +0100

    add axiom logs for clerk trpc endpoint

--- apps/dashboard-app/server/routers/auth.ts
+++ apps/dashboard-app/server/routers/auth.ts
@@ -2,12 +2,15 @@ import { router, publicProcedure } from '../trpc';
 import { z } from 'zod';
 import { TRPCError } from '@trpc/server';
 import { hashPassword } from '../../utils/functions';
+import { log } from 'next-axiom'
 
 const authRouter = router({
 	clerk: publicProcedure.input(Object).mutation(async ({ input, ctx }) => {
 		try {
 			console.log('-----------------------------------------------');
 			console.log(input)
+			log.info('clerk webhook', { customerId: 32423, auth: 'session' })
+			log.info(input)
 			console.log('-----------------------------------------------');
 			return { message: "Success" };
 		} catch (err) {
