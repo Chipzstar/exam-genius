b70a78b7e5303b3645599c96a1f9d84cd71f651b
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-08 00:21:29 +0100

    set /api/clerk as a public route in middleware.ts

--- apps/dashboard-app/middleware.ts
+++ apps/dashboard-app/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from 'next/server';
 import { getAuth, withClerkMiddleware } from '@clerk/nextjs/server';
 
 // Set the paths that don't require the user to be signed in
-const publicPaths = ['/signup*', '/login*'];
+const publicPaths = ['/signup*', '/login*', '/api/clerk/*'];
 
 const isPublic = (path: string) => {
 	return publicPaths.find(x => path.match(new RegExp(`^${x}$`.replace('*$', '($|/)'))));
@@ -14,13 +14,12 @@ export default withClerkMiddleware((request: NextRequest) => {
 	if (isPublic(request.nextUrl.pathname)) {
 		return NextResponse.next();
 	}
-	// if the user is not signed in redirect them to the sign in page.
+	// if the user is not signed in redirect them to the sign-in page.
 	const { userId } = getAuth(request);
 	console.log("USER_ID:", userId)
 
 	if (!userId) {
 		// redirect the users to /pages/sign-in/[[...index]].ts
-
 		const signInUrl = new URL('/login', request.url);
 		signInUrl.searchParams.set('redirect_url', request.url);
 		return NextResponse.redirect(signInUrl);
