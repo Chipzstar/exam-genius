331b4a4bdb90d28b515b9b38343122ee8b0c79dc
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-08 13:37:47 +0100

    fix prisma issues with user schema

--- apps/dashboard-app/server/routers/auth.ts
+++ apps/dashboard-app/server/routers/auth.ts
@@ -17,40 +17,7 @@ const authRouter = router({
 			console.error(err);
 			throw new TRPCError({code: "UNPROCESSABLE_CONTENT", message: err.message});
 		}
-	}),
-	signup: publicProcedure
-		.input(
-			z.object({
-				full_name: z.string(),
-				email: z.string(),
-				password: z.string(),
-				role: z.string(),
-				year: z.string()
-			})
-		)
-		.mutation(async ({ input, ctx }) => {
-			try {
-				// hash the user's password
-				const hashed_password = await hashPassword(input.password);
-				// store the user in db
-				const user = await ctx.prisma.user.create({
-					data: {
-						email: input.email,
-						password: hashed_password,
-						full_name: input.full_name,
-						year: input.year,
-						role: input.role
-					}
-				});
-				console.log('-----------------------------------------------');
-				console.log(user);
-				console.log('-----------------------------------------------');
-				return { ...user, hashed_password };
-			} catch (e) {
-				console.error(e);
-				throw new TRPCError({ code: 'BAD_REQUEST', message: e.message });
-			}
-		})
+	})
 });
 
 export default authRouter;
