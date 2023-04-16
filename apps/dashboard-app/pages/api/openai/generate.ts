76c4e5adb9e72d8923ebb25171f0cf60b9a6f7ec
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-17 00:10:57 +0100

    modify openai prompt

--- apps/dashboard-app/pages/api/openai/generate.ts
+++ apps/dashboard-app/pages/api/openai/generate.ts
@@ -20,7 +20,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 						role: 'user',
 						content:
 							`Please generate a new sample past paper for the upcoming A-level ${exam_board} ${subject} ${course} ${paper_name} exam in June` +
-							`The questions generated should be based on A-level ${exam_board} ${subject} ${paper_name} past paper questions from the years 2018 to 2022.` +
+							`The questions generated should be based on A-level ${exam_board} ${subject} ${course} past paper questions from the years 2018 to 2022.` +
 							`The past paper should have ${num_questions} questions. The total mark for this paper is ${num_marks}. Questions should all be relevant to the ` +
 							`${exam_board} ${subject} ${course} specification. For each question, please format it using HTML markup. For example, "'1. Simplify: \n' +
       'a) (2 + sqrt(3)) (2 - sqrt(3))\n' +
