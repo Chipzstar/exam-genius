e4277526e3ad074085d071151ced8fb7a9685704
Chipzstar <chipzstar.dev@googlemail.com>
2023-04-25 16:50:19 +0100

    remove backend nx project

--- apps/backend/src/app/modules/paper/paper.route.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyInstance } from 'fastify';
-import { generatePaper } from './paper.controller';
-
-async function paperRoutes(server: FastifyInstance) {
-	server.post('/generate', generatePaper);
-}
-
-export default paperRoutes;
