fb95190d6a2f48c5b227edcf9c7f6c36853f78e9
Chipzstar <chipzstar.dev@googlemail.com>
2026-05-11 11:11:58 +0100

    Fix typo in exam subject name
    Corrects a typographical error in the name of an A-Level subject paper from "Turing Points" to "Turning Points".
    
    Also updates the alt text for an image within the exam board card component to better reflect its content.

--- libs/shared/ui/src/lib/exam-board-card/exam-board-card.tsx
+++ libs/shared/ui/src/lib/exam-board-card/exam-board-card.tsx
@@ -12,7 +12,7 @@ export function ExamBoardCard(props: ExamBoardCardProps) {
 	return (
 		<Card component='label' shadow='sm' radius='xs' withBorder mah={400} className={classes.card}>
 			<Stack justify='center' align='center'>
-				<Image src={props.src} alt='subject-icon' width={300} height={300} />
+				<Image src={props.src} alt='exam-board-icon' width={300} height={300} />
 			</Stack>
 			<Group justify='right'>
 				<Radio value={props.value} />
